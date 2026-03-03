464ed8f99f652ba3e4198224fce916b912bef32b
IshitwoKhanra <ishitwokhanra2006@gmail.com>
2026-03-04 01:51:44 +0530

    Testing Ci by breaking

--- test_utils.py
+++ test_utils.py
@@ -1,7 +1,7 @@
 from utils import add_numbers,multiply_numbers
 
 def test_add():
-    assert add_numbers(2, 3) == 5
+    assert add_numbers(2, 3) == 10
 
 def test_add_negative():
     assert add_numbers(-1, -1) == -2
